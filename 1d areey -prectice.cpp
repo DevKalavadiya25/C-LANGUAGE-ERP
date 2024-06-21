e53e5f5bd48542cf7f63257f606943a4e36a9c82
Devkalavadiya25 <devangkalavadiya25@gmail.com>
2024-06-21 10:26:15 +0530

    add

---
+++ 1d areey -prectice.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+
+int a[5]={21,22,26,24,25};
+  
+   a[2]={23};
+   
+//   printf("%d",a[2]);
+   
+   for(int i=5; i>=0; i--){
+   	
+   	printf("%d\n",a[i]);
+   }
+   
+}
---
+++ 1d arey prectice-2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+	char a[6] = {'D','E','V','A','N','G'};
+	
+//	scanf("%c",&a[]);
+	
+	for(char i=0; i<=6; i++){
+		
+		printf("%c",a[i]);
+	}
+	
+}
